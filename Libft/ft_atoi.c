d9157f4c7067b726706f9a7532013ec82a6eac91
Jahongir Abdujalilov <jabdujalilov24@fed.idserve.net>
2026-08-09 18:06:33 +0200

    ft_atoi done, comments left

---
+++ Libft/ft_atoi.c
@@ -0,0 +1,36 @@
+//#include <stdio.h>
+//#include <string.h>
+//#include <stdlib.h>
+
+int ft_atoi(const char *str)
+{
+  int i = 0;
+  int mc = 1;
+  int res = 0;
+
+  while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+    i++;
+
+  if (str[i] == '-' || str[i] == '+') 
+  {
+    if (str[i] == '-')
+      mc = -1;
+    i++;
+  }
+
+  while (str[i] >= '0' && str[i] <= '9')
+  {
+    res = (res * 10) + (str[i] - '0');
+    i++;
+  }
+
+  return (res * mc);
+}
+
+/*int main(void)
+{
+  char test[] = "   --22";
+
+  printf("built in function: %i\n", atoi(test)); // res = 0
+  printf("beloved function: %i\n", ft_atoi(test)); // res = -22
+}*/
