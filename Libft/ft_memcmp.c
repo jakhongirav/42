e48240d71d0803f18357a4e0f37f802522021ced
Jahongir Abdujalilov <jakhongir.av@gmail.com>
2026-08-11 00:17:14 +0200

    ft_memcmp done

---
+++ Libft/ft_memcmp.c
@@ -0,0 +1,15 @@
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+  size_t i = 0;
+  const unsigned char *str1 = (const unsigned char *)s1;
+  const unsigned char *str2 = (const unsigned char *)s2;
+
+  while (i < n)
+  {
+    if (str1[i] != str2[i])
+      return (str1[i] - str2[i]);
+    i++;
+  }
+
+  return (0);
+}
