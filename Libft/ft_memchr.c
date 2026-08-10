c34389ec11c8d5ea69cdf35f90cb6a0597272140
Jahongir Abdujalilov <jakhongir.av@gmail.com>
2026-08-10 21:29:58 +0200

    ft_memchr done

---
+++ Libft/ft_memchr.c
@@ -0,0 +1,14 @@
+void  *ft_memchr(const void *s, int c, size_t n)
+{
+  size_t i = 0; 
+  const unsigned char *str = (const unsigned char *)s;
+
+  while (i < n)
+  {
+    if (str[i] == (unsigned char)c)
+      return ((void *)&str[i]);
+    i++;
+  }
+
+  return (NULL);
+}
