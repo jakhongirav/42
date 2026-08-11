cd0b5db515717260b4c50b22a57f38fca1b1642b
Jahongir Abdujalilov <jakhongir.av@gmail.com>
2026-08-12 00:19:00 +0200

    ft_calloc fixed

--- Libft/ft_calloc.c.c
+++ Libft/ft_calloc.c.c
@@ -1,9 +1,17 @@
 #include <stdlib.h>
+#include <stdint.h>
 
 void  *ft_calloc(size_t count, size_t size)
 {
-  char *ptr = malloc(count * size);
   size_t i = 0;
+  char *ptr;
+
+  if (size != 0 && count > SIZE_MAX / size)
+      return (NULL); 
+
+  ptr = malloc(count * size);
+  if (!ptr)
+    return (NULL);
 
   while (i < (count * size))
   {
