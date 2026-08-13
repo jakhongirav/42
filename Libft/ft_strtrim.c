d1a56d3a574f709aa5008dc196acf2e76e3aea55
Jahongir Abdujalilov <jakhongir.av@gmail.com>
2026-08-13 16:14:46 +0200

    ft_strtrim done

---
+++ Libft/ft_strtrim.c
@@ -0,0 +1,30 @@
+#include "libft.h"
+
+char  *ft_strtrim(char const *s1, char const *set)
+{
+  size_t i;
+  size_t start;
+  size_t end;
+  char *res;
+
+  start = 0;
+  while (s1[start] && ft_strchr(set, s1[start]))
+    start++;
+
+  end = ft_strlen(s1);
+  i = 0;
+  while (end > start && ft_strchr(set, s1[end - 1]))
+    end--;
+
+  res = malloc(end - start + 1);
+  if (!res)
+    return (NULL);
+
+  i = 0;
+  while (start < end)
+    res[i++] = s1[start++];
+
+  res[i] = '\0';
+
+  return (res);
+}
--- Libft/libft.h
+++ Libft/libft.h
@@ -31,7 +31,4 @@ int     ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t  ft_strlen(const char *s);
 char    *ft_strdup(const char *s1);
 
-char    *ft_substr(char const *s, unsigned int start, size_t len);
-char    *ft_strjoin(char const *s1, char const *s2);
-
 #endif
