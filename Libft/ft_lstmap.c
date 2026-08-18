8807af075d7a996858073146ba3f86d2f01612fa
Jahongir Abdujalilov <jakhongir.av@gmail.com>
2026-08-18 23:37:34 +0200

    ft_lstmap done

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -14,13 +14,23 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*res;
+	t_list	*new_list;
+  t_list  *new_node;
+  void    *new_content;
 
-	res = ft_lstnew(lst);
-	while (lst)
+  new_list = NULL;
+  while (lst)
 	{
-		ft_lstiter(lst, f);
-		res->content = lst->content;
-		lst = lst->next;
-	}
-
+    new_content = f(lst->content);
+    new_node = ft_lstnew(new_content);
+    if (!new_node)
+    {
+      del(new_content);
+      ft_lstclear(&new_list, del);
+      return (NULL);
+    }
+    ft_lstadd_back(&new_list, new_node);
+    lst = lst->next;
+  }
+  return (new_list);
+}
